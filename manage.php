d8dd59b046c44c1c55898462fa84a71094d9b47b
Ibrahim Mohamed <105929854@student.swin.edu.au>
2025-05-22 12:55:35 +1000

    Connecting to SQL

--- manage.php
+++ manage.php
@@ -4,3 +4,16 @@ session_start();
 // Include the database connection variables
 include_once('settings.php');
 
+// If user is not logged in, redirect to login page
+if (!isset($_SESSION['manager_logged_in'])) {
+    header("Location: login.php");
+    exit;
+}
+
+// Connect to the MySQL database
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+if (!$conn) {
+    // If connection fails, show error and stop script
+    die("Connection failed: " . mysqli_connect_error());
+}
+
